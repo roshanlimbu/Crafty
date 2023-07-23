22f0cb718cfc60ed5a6cbe67544d652314b59771
Roshan Limbu <topazkayle@gmail.com>
2023-07-23 20:15:11 +0545

    updated

---
+++ init.lua
@@ -0,0 +1,20 @@
+require('roshan.base')
+require('roshan.highlights')
+require('roshan.maps')
+require('roshan.plugins')
+require('roshan.windows')
+
+local has = vim.fn.has
+local is_mac = has "macunix"
+local is_win = has "win32"
+local is_wsl = has "wsl"
+
+if is_mac == 1 then
+  require('roshan.macos')
+end
+if is_win == 1 then
+  require('roshan.windows')
+end
+if is_wsl == 1 then
+  require('roshan.wsl')
+end
---
+++ lua/roshan/plugins.lua
@@ -0,0 +1,81 @@
+local status, packer = pcall(require, "packer")
+if (not status) then
+  print("Packer is not installed")
+  return
+end
+
+vim.cmd [[packadd packer.nvim]]
+
+packer.startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use {
+    'svrana/neosolarized.nvim',
+    requires = { 'tjdevries/colorbuddy.nvim' }
+  }
+  -- colorscheme
+  use 'morhetz/gruvbox'
+  use 'LunarVim/Colorschemes'
+
+
+  use 'nvim-lualine/lualine.nvim' -- Statusline
+  use 'nvim-lua/plenary.nvim'     -- Common utilities
+  use 'onsails/lspkind-nvim'      -- vscode-like pictograms
+  use 'jose-elias-alvarez/null-ls.nvim' -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
+  use 'rafamadriz/friendly-snippets'
+  use 'akinsho/toggleterm.nvim'
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v1.x',
+    requires = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
+
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/cmp-path' },
+      { 'saadparwaiz1/cmp_luasnip' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-nvim-lua' },
+
+      -- Snippets
+      { 'L3MON4D3/LuaSnip' },
+      { 'rafamadriz/friendly-snippets' },
+    }
+  }
+
+
+
+
+  use 'glepnir/lspsaga.nvim' -- LSP UIs
+  use 'L3MON4D3/LuaSnip'
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+  }
+  use 'kyazdani42/nvim-web-devicons' -- File icons
+  use 'nvim-telescope/telescope.nvim'
+  use 'nvim-telescope/telescope-file-browser.nvim' --telescope file browser
+  use 'windwp/nvim-autopairs' -- autopairs
+  use 'windwp/nvim-ts-autotag'
+  use { 'numToStr/Comment.nvim',
+    requires = {
+      'JoosepAlviste/nvim-ts-context-commentstring'
+    }
+  }
+  use 'norcalli/nvim-colorizer.lua'
+  use 'folke/zen-mode.nvim'
+  use({
+    "iamcco/markdown-preview.nvim",
+    run = function() vim.fn["mkdp#util#install"]() end,
+  })
+  use 'akinsho/nvim-bufferline.lua' --bufferline
+  -- use 'github/copilot.vim'
+  use { 'nvim-tree/nvim-tree.lua', commit = '9c97e6449b0b0269bd44e1fd4857184dfa57bb4c' }
+
+  use 'tpope/vim-fugitive' -- vim fugitive
+  -- use 'lewis6991/gitsigns.nvim'
+  -- use 'dinhhuy258/git.nvim' -- For git blame & browse
+end)
